0b3f2d6e7085ace178d79bd262ac5c1a6a57644b
daninna422-ops <daninna422@gmail.com>
2026-08-07 17:57:13 +0100

    Initialize Firebase app and authentication
    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDaeiQk55PVBPdxVm1T6TbTpk6TadZzE3c",
+  authDomain: "center-app-ai.firebaseapp.com",
+  projectId: "center-app-ai",
+  storageBucket: "center-app-ai.firebasestorage.app",
+  messagingSenderId: "885493707552",
+  appId: "1:885493707552:web:3987b74d77771cd3031d45",
+  measurementId: "G-1DJ1RKQ074"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
