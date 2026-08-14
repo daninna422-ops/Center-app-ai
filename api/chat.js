fea466bf482663f5fbc991c8ddfb7892c2d7526d
center app ai <daninna422@gmail.com>
2026-08-14 22:31:04 +0100

    Reduce maxOutputTokens to 512 in chat.js
    update chat.js

--- api/chat.js
+++ api/chat.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
           ],
           generationConfig: {
             temperature: 0.7,
-            maxOutputTokens: 2048
+            maxOutputTokens: 512
           }
         })
       }
